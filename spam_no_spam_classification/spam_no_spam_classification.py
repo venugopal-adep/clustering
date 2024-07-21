671ab036be97644d15139857ff2fded6190d77da
venugopal-adep <92349362+venugopal-adep@users.noreply.github.com>
2024-07-21 17:19:51 +0530

    Update spam_no_spam_classification.py

--- spam_no_spam_classification/spam_no_spam_classification.py
+++ spam_no_spam_classification/spam_no_spam_classification.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as npimport streamlit as st
-import pandas as pd
 import numpy as np
 import plotly.express as px
 from sklearn.feature_extraction.text import CountVectorizer
